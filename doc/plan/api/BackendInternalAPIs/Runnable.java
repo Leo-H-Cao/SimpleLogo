88dd0783eaa36d090ca2e5588bb7255aa95876b6
Alex Bildner <alexander.bildner@duke.edu>
2022-02-13 23:01:47 -0500

    split up internal API logically

---
+++ doc/plan/api/BackendInternalAPIs/Runnable.java
@@ -0,0 +1,20 @@
+public interface Runnable {
+
+  /**
+   * Run lexical analyzer on the user inputted instruction
+   * and return the result in the form of a
+   * LexResult object
+   *
+   * @param instruction the String which contains the user inputted instruction
+   * @return LexResult object which contains the result of the Lexical Analysis in a form that
+   * is understandable by the parser
+   */
+  public LexResult runLexicalAnalyzer(String instruction);
+
+  /**
+   * Adds queue of Turtles to History
+   * @return True if success, False if failure
+   */
+  public boolean addTurtleQueueToHistory(Queue<Turtle>);
+
+}
