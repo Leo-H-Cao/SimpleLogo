feb7061865472341fd0a2149a1a6ba19a684bf0e
Alex Bildner <alexander.bildner@duke.edu>
2022-02-14 02:43:42 -0500

    outlined my use cases

---
+++ doc/plan/usecases/AlexBildnerUseCases.java
@@ -0,0 +1,30 @@
+public class AlexBildnerUseCases {
+
+  /**
+   * This usecase indicates how our program would handle checking if the program
+   * was currently using English as the commmand language and if it was, changing the
+   * command language to French
+   */
+  public void firstUseCase()  {
+
+  }
+
+  /**
+   * This usecase indicates how our program's controller would handle checking the correctness
+   * of the syntax of a user entered instruction, and upon realizing the incorrect syntax
+   * of this instruction, getting the error message and sending it to the Display
+   */
+  public void secondUseCase()  {
+
+  }
+
+  /**
+   * This usecase indicates how our program would handle getting the current command language
+   * of the program, getting the command history of the program, and getting the user defined
+   * variables of the program – and then printing all of these to the console (perhaps for
+   * debugging purposes).
+   */
+  public void thirdUseCase()  {
+
+  }
+}
