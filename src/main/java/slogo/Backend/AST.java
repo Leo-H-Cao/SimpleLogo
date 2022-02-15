91089b93ab72639cd0895f050a37319a8335fb9a
Alex Bildner <alexander.bildner@duke.edu>
2022-02-15 00:16:45 -0500

    created backend branch. This branch will house development of the backend of the SLogo IDE. Also added comment to AST.java

--- src/main/java/slogo/Backend/AST.java
+++ src/main/java/slogo/Backend/AST.java
@@ -1,5 +1,9 @@
 package slogo.Backend;
 
+/**
+ * This Class will define the Abstract Sytax Tree Object. It will be created
+ * after an instruction has been passes through the Lexical Analyzer.
+ **/
 public class AST {
 
 }
