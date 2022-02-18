ae715be77cb91f4443e9cbc77fe25c2bc76928c4
Marcus Ortiz <mjo24@duke.edu>
2022-02-18 11:10:14 -0500

    Adding a title and refactoring

--- src/main/java/slogo/Frontend/MainUI.java
+++ src/main/java/slogo/Frontend/MainUI.java
@@ -12,11 +12,13 @@ public class MainUI {
   private Pane layout;
   private Scene myScene;
   private MenuBarUI myMenuBar;
+  private OutputView myOutput;
 
   public MainUI(Stage myStage) {
     layout = new VBox();
     myScene = new Scene(layout,800, 800);
     myMenuBar = new MenuBarUI();
+    myOutput = new OutputView();
     layout.getChildren().add(myMenuBar.getMenuBar());
     layout.prefWidthProperty().bind(myScene.widthProperty().multiply(0.80));
     myStage.setResizable(false);
@@ -25,6 +27,12 @@ public class MainUI {
     layout.getChildren().add(testTurtle.getTurtleNode());
     Animation animation = testTurtle.makeAnimation();
     animation.play();
+  }
+  private void addingLayoutNodes(){
+    myMenuBar = new MenuBarUI();
+    myOutput = new OutputView();
+    layout.getChildren().add(myMenuBar.getMenuBar());
+    layout.getChildren().add(myOutput.getOutputView());
   }
 
   public Scene getScene(){
