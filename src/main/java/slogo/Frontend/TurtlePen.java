f7d9e644b96bfb7f4b226ef2523a240383f1ecb6
Marcus Ortiz <mjo24@duke.edu>
2022-02-18 20:37:31 -0500

    added pen visibility and changed turtle view stuff

--- src/main/java/slogo/Frontend/TurtlePen.java
+++ src/main/java/slogo/Frontend/TurtlePen.java
@@ -1,13 +1,19 @@
 package slogo.Frontend;
 
+import javafx.scene.image.ImageView;
 import slogo.FrontendInternalAPIs.DisplayPen;
 
 public class TurtlePen implements DisplayPen {
+  public static final double TRANSPARENT = 0;
+  public static final double VISIBLE = 1;
+  public static final String PEN_IMAGE_PATH = "";
+
+  private ImageView penView;
 
   public TurtlePen(){
 
   }
-
+  // Going to have to find different pen color images (or make them) unless javafx can support vector images
   @Override
   public void setPenColor(){
 
@@ -20,7 +26,10 @@ public class TurtlePen implements DisplayPen {
 
   @Override
   public void setPenVisible(){
-
+    penView.setOpacity(VISIBLE);
+  }
+  public void setPenInvisible(){
+    penView.setOpacity(TRANSPARENT);
   }
 
 }
