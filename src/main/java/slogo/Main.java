0e459160912ee6b5bf95ee5f4728c3c2fbbcac6a
Edison Ooi <edison.ooi@duke.edu>
2022-02-19 18:40:56 -0500

    Fixed setupNewSLogo to initialize a View instead of a MainUI

--- src/main/java/slogo/Main.java
+++ src/main/java/slogo/Main.java
@@ -66,7 +66,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         controller = new SLogoController(primaryStage);
-        controller.setupNewSlogo();
+        controller.setupNewSLogo();
     }
 
     /**
--- src/main/java/slogo/SLogoController.java
+++ src/main/java/slogo/SLogoController.java
@@ -5,29 +5,30 @@ import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import slogo.Backend.InitializationState;
 import slogo.Backend.ModelState;
+import slogo.Frontend.CommandInput;
+import slogo.Frontend.CommandOutput;
 import slogo.Frontend.MainUI;
+import slogo.Frontend.TurtleView;
+import slogo.Frontend.View;
+import slogo.Frontend.ViewUserDefined;
 
 public class SLogoController {
   public static final String SLOGO_PROGRAM_TITLE = "SLogo";
   private ModelState model;
+  private CommandInput commandInputter;
+  private CommandOutput commandOutputter;
+  private TurtleView turtleView;
+  private ViewUserDefined userDefinedAttributesUpdater;
   private Stage myStage;
 
   public SLogoController(Stage stage) {
     model = new ModelState();
     myStage = stage;
-
-
-
   }
 
-  public void setupNewSlogo() {
+  public void setupNewSLogo() {
     // Set up and show stage
-    MainUI gui = new MainUI(myStage);
-    Scene mainScene = gui.getScene();
-    mainScene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
-    myStage.setScene(mainScene);
-    myStage.setTitle(SLOGO_PROGRAM_TITLE);
-    myStage.show();
+    View mainView = new View(myStage, e -> handleKeyInput(e.getCode()));
 
     // initialize window to set parameters
     // frontend stuff to get language + other initial parameters that the backend needs to know
