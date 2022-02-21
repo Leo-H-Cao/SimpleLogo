3c99378ce97abb9c4236b2b5b44a1fe45de2474e
Alex Bildner <alexander.bildner@duke.edu>
2022-02-21 01:07:45 -0500

    added CoordinateTest Skeleton

---
+++ src/test/java/slogo/Backend/TurtleState/CoordinateTest.java
@@ -0,0 +1,27 @@
+package slogo.Backend.TurtleState;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class CoordinateTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void getX() {
+  }
+
+  @ParameterizedTest()
+  void getY() {
+  }
+}
