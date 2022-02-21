a96e80cbad14300f2b880a71a71dc13f4a40a8c1
Jed Yang <zy111@duke.edu>
2022-02-21 00:26:29 -0500

    fixed two minor compilation errors temporarily: return null in TokenScanner, comment out two duplicated methods in Updater

--- src/main/java/slogo/Backend/LexicalAnalyzer/TokenScanner.java
+++ src/main/java/slogo/Backend/LexicalAnalyzer/TokenScanner.java
@@ -41,5 +41,6 @@ public class TokenScanner {
         throw new InvalidTokenException(exceptionMessage);
       }
     }
+    return null; //temporarily added to compile
   }
 }
--- src/main/java/slogo/Backend/TurtleState/Updater.java
+++ src/main/java/slogo/Backend/TurtleState/Updater.java
@@ -31,6 +31,7 @@ public class Updater implements Updates {
     return null;
   }
 
+
   /**
    * Iterate through the AST for numInstructions instructions and return the final turtle state
    *
@@ -40,19 +41,22 @@ public class Updater implements Updates {
    * @param numInstructions the number of instructions to execute
    * @return the turtle in its final state
    */
+  /*
   @Override
   public Turtle step(Turtle initialState, AST tree, ASTNode current, int numInstructions) {
     return null;
   }
-
+*/
   /**
    * Add turtle to history of turtles. .
    *
    * @param current the turtle to be added
    * @return true if the addition was sucessful, false if an error occured (e.g. stack overflow)
    */
+  /*
   @Override
   public Boolean addTurtleToHistory(Turtle current) {
     return null;
   }
+   */
 }
