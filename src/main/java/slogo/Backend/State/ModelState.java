e571848c84a5053c98dc7c805c254a960460e6e6
Edison Ooi <edison.ooi@duke.edu>
2022-02-27 18:21:00 -0500

    Combined AccessesState and ControlsTurtle API's into one API StateManager, moved postInstruction() into ModelState

--- src/main/java/slogo/Backend/State/ModelState.java
+++ src/main/java/slogo/Backend/State/ModelState.java
@@ -1,6 +1,7 @@
 package slogo.Backend.State;
 
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Locale;
@@ -8,20 +9,51 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import slogo.Backend.FileReadingException;
 import slogo.Backend.InstructionHistory;
+import slogo.Backend.LexicalAnalyzer.InvalidTokenException;
+import slogo.Backend.LexicalAnalyzer.LexResult;
 import slogo.Backend.Preferences;
+import slogo.Backend.Result;
+import slogo.Backend.SyntaxParser.ASTMaker;
+import slogo.Backend.SyntaxParser.Operator;
 import slogo.Backend.TurtleState.Turtle;
-import slogo.BackendExternalAPIs.AccessesState;
+import slogo.BackendExternalAPIs.StateManager;
 import slogo.BackendExternalAPIs.Initialiazable;
 import slogo.BackendExternalAPIs.ModifiesModelState;
 import slogo.Backend.className;
 
-public class ModelState implements Initialiazable, ModifiesModelState, AccessesState {
+public class ModelState implements Initialiazable, ModifiesModelState, StateManager {
   private CommandLanguage commandLanguage;
   private Turtle turtle;
   private Tracks tracks;
-  private TurtleHistory History;
+  private TurtleHistory history;
   private UserVariables userVariables;
   private UserCommands userCommands;
+
+  /**
+   * This methods sends an instruction entered by the user to the backend.
+   * <p>
+   * If the instruction is "successful" meaning it is syntactically and semantically valid, then
+   * this method will return a Deque of Turtle objects which in their totality execute the command
+   * from the user by the end of the Deque, but where adjacent Turtle objects in the Deque are
+   * separated by one basic command.
+   * <p>
+   * If the instruction is "unsuccessful," meaning that it is either syntactically and/or
+   * semantically invalid or unsuccessful for any other reason, then this method will throw an
+   * InvalidTokenException or an instance of one of its subclasses.
+   *
+   * @param instructionText a String with the instruction text entered by the user to be executed
+   * @return Deque<Turtle> which has the Turtle objects created by executing the instruction, each
+   * subsequent Turtle object separated by only one basic command.
+   */
+  @Override
+  public Result postInstruction(String instructionText)
+      throws InvalidTokenException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    LexResult lexedString = new LexResult(instructionText);
+    ASTMaker astMaker = new ASTMaker(lexedString.getTokens());
+    Operator root =  astMaker.parse();
+    Result res = new Result(root.getRetVal(history), history.getTurtleHistory().getLast());
+    return res;
+  }
 
 
   /**
