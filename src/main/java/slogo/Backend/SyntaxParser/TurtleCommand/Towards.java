805bf8c1ee776ad517ee46c64eab1ad9105ed605
Edison Ooi <edison.ooi@duke.edu>
2022-02-28 02:08:34 -0500

    Added a second argument to Towards

--- src/main/java/slogo/Backend/SyntaxParser/TurtleCommand/Towards.java
+++ src/main/java/slogo/Backend/SyntaxParser/TurtleCommand/Towards.java
@@ -9,19 +9,22 @@ public class Towards extends Operator {
 
   public Towards(int seqNum){
     super(seqNum);
-    myNumArgs = 1;
+    myNumArgs = 2;
   }
 
-  public Towards(Operator op1){
+  public Towards(Operator op1, Operator op2){
     super(0);
     arguments.add(op1);
+    arguments.add(op2);
   }
 
   public double getRetVal(TurtleHistory history)
       throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
     double retVal = arguments.get(0).getRetVal(history);
+    double retVal2 = arguments.get(1).getRetVal(history);
     ArrayList<Double> argList = new ArrayList<>();
     argList.add(retVal);
+    argList.add(retVal2);
     history.addTurtleThroughTransformation("Towards", argList);
     return retVal;
   }
