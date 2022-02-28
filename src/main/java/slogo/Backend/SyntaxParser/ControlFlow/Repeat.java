b5906f623f4e090abc55f194954be0356cba9d5f
Jed Yang <zy111@duke.edu>
2022-02-28 02:14:33 -0500

    finished Repeat operator

--- src/main/java/slogo/Backend/SyntaxParser/ControlFlow/Repeat.java
+++ src/main/java/slogo/Backend/SyntaxParser/ControlFlow/Repeat.java
@@ -1,14 +1,21 @@
 package slogo.Backend.SyntaxParser.ControlFlow;
 
+import java.lang.reflect.InvocationTargetException;
 import slogo.Backend.State.TurtleHistory;
+import slogo.Backend.SyntaxParser.LogoList;
 import slogo.Backend.SyntaxParser.Operator;
 
 public class Repeat extends Operator {
   public Repeat(int seqNum){
     super(seqNum);
   }
-  public double getRetVal(TurtleHistory history){
-    return 0;
+  public double getRetVal(TurtleHistory history)
+      throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    int numRepetitions = (int) arguments.get(0).getRetVal(history);
+    for(int i=0; i<numRepetitions; i++){
+      double retVal = arguments.get(1).getRetVal(history);
+    }
+    return retVal;
   }
 
 }
