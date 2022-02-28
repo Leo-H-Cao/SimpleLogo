567708651548bc7429fbd8a097513924624ea279
Edison Ooi <edison.ooi@duke.edu>
2022-02-28 02:08:18 -0500

    Added tests for executing TurtleCommands

--- src/test/java/slogo/SLogoControllerTest.java
+++ src/test/java/slogo/SLogoControllerTest.java
@@ -27,7 +27,43 @@ public class SLogoControllerTest extends DukeApplicationTest {
 
   @Test
   void testValidCommand() {
-    writeInputTo(commandInputTextArea, "fd 50");
+    writeInputTo(commandInputTextArea, "Forward 50");
+    clickOn(lookup("#ExecuteButton").query());
+  }
+
+  @Test
+  void testForwardExecution() {
+    writeInputTo(commandInputTextArea, "Forward 100");
+    clickOn(lookup("#ExecuteButton").query());
+  }
+
+  @Test
+  void testBackExecution() {
+    writeInputTo(commandInputTextArea, "Back 100");
+    clickOn(lookup("#ExecuteButton").query());
+  }
+
+  @Test
+  void testLeftExecution() {
+    writeInputTo(commandInputTextArea, "Left 1.5");
+    clickOn(lookup("#ExecuteButton").query());
+  }
+
+  @Test
+  void testRightExecution() {
+    writeInputTo(commandInputTextArea, "Right 1.5");
+    clickOn(lookup("#ExecuteButton").query());
+  }
+
+  @Test
+  void testSetHeadingExecution() {
+    writeInputTo(commandInputTextArea, "Setheading 0.5");
+    clickOn(lookup("#ExecuteButton").query());
+  }
+
+  @Test
+  void testTowardsExecution() {
+    writeInputTo(commandInputTextArea, "Towards 100 200");
     clickOn(lookup("#ExecuteButton").query());
   }
 
@@ -35,7 +71,7 @@ public class SLogoControllerTest extends DukeApplicationTest {
   void testTextAreaClearing() {
     writeInputTo(commandInputTextArea, " \n");
     clickOn(lookup("#ExecuteButton").query());
-    assertEquals(commandInputTextArea.getText().length(), 0);
+    assertEquals(0, commandInputTextArea.getText().length());
   }
 
   @Test
