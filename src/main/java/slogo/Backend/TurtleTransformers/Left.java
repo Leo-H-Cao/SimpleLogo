6f6208a4a09f42c3e623293c67b08ba5a8ae0321
Jed Yang <zy111@duke.edu>
2022-02-28 00:16:18 -0500

    removed usages of headingDirection, direction is now only represented with Direction objects

--- src/main/java/slogo/Backend/TurtleTransformers/Left.java
+++ src/main/java/slogo/Backend/TurtleTransformers/Left.java
@@ -17,7 +17,7 @@ public class Left implements TurtleTransformer {
 
   @Override
   public Map<String, Double> transform() {
-    double turtleAngle = myOldTurtle.getHeadingDirection();
+    double turtleAngle = myOldTurtle.getDirection().getDirectionInRadians();
     double newTurtleAngle = turtleAngle + myParams.get(0);
     return Map.of(Turtle.DIRECTION, newTurtleAngle);
   }
