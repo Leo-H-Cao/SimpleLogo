2714fa2165af30a322d0049209c900fd2161b25d
Leo Cao <lhc22@duke.edu>
2022-03-01 18:13:34 -0500

    display error in command output

--- src/main/java/slogo/Frontend/CommandOutput.java
+++ src/main/java/slogo/Frontend/CommandOutput.java
@@ -1,18 +1,26 @@
 package slogo.Frontend;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 import slogo.Backend.ErrorText;
 import slogo.FrontendExternalAPIs.UpdateCommandResult;
 
 public class CommandOutput implements UpdateCommandResult {
+  private Alert errorAlert;
   public CommandOutput() {
-    TextField commands = new TextField();
+    errorAlert = new Alert(AlertType.ERROR);
   }
+
   /**
    * show error if command is not valid
    *
    * @param error
    */
   @Override
-  public void displayError(ErrorText error) {}
+  public void displayError(ErrorText error) {
+    errorAlert.setHeaderText("Input not valid");
+    errorAlert.setContentText(error.getText());
+    errorAlert.showAndWait();
+  }
 }
