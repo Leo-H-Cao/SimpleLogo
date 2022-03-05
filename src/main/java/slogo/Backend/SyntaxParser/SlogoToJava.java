2a53ebf7c077a61e3ac3473f9ae4c895393917cd
Alex Bildner <alexander.bildner@duke.edu>
2022-03-05 03:45:09 -0500

    quick fix

---
+++ src/main/java/slogo/Backend/SyntaxParser/SlogoToJava.java
@@ -0,0 +1,23 @@
+package slogo.Backend.SyntaxParser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class SlogoToJava {
+  private static Map<String,String> conversionMap = SlogoToJava.loadConversionMap();
+
+  private static Map<String, String> loadConversionMap() {
+    ResourceBundle resourceBundle = ResourceBundle.getBundle("slogo/SlogoToJava");
+    Map<String,String> conversion = new HashMap<>();
+    for (String slogoName: resourceBundle.keySet()) {
+      conversion.put(slogoName, resourceBundle.getString(slogoName));
+    }
+    return conversion;
+  }
+
+  public static String convertSlogoNameToJavaName(String slogoName) {
+    return SlogoToJava.conversionMap.getOrDefault(slogoName, slogoName);
+  }
+
+}
