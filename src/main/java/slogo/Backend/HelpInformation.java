e2c025352435aea9f64f1af1e6b33d817d6dab7a
Edison Ooi <edison.ooi@duke.edu>
2022-03-05 00:53:13 -0500

    More javadocs

--- src/main/java/slogo/Backend/HelpInformation.java
+++ src/main/java/slogo/Backend/HelpInformation.java
@@ -1,12 +1,24 @@
 package slogo.Backend;
 
+/**
+ * This class contains the String representation of the help requested by a user.
+ *
+ * @author Edison Ooi
+ */
 public class HelpInformation {
   private String info;
 
+  /**
+   * Class constructor.
+   * @param info String representing help info
+   */
   public HelpInformation(String info) {
     this.info = info;
   }
 
+  /**
+   * @return String representing help info
+   */
   protected String getInfo() {
     return info;
   }
--- src/main/java/slogo/Backend/InvalidCommandLanguageException.java
+++ src/main/java/slogo/Backend/InvalidCommandLanguageException.java
@@ -1,5 +1,11 @@
 package slogo.Backend;
 
+/**
+ * This Exception is thrown whenever an invalid command language - one that does not appear in the
+ * CommandLanguage enum - is used.
+ *
+ * @author Edison Ooi
+ */
 public class InvalidCommandLanguageException extends Exception {
   public InvalidCommandLanguageException(String message) {
     super(message);
