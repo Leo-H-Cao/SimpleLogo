5af6a13b288a7b3cea7284318e9cf6072c5a0d89
Alex Bildner <alexander.bildner@duke.edu>
2022-03-04 23:06:28 -0500

    updated tests

--- src/main/java/slogo/Backend/LexicalAnalyzer/TokenFactory.java
+++ src/main/java/slogo/Backend/LexicalAnalyzer/TokenFactory.java
@@ -1,11 +1,12 @@
 package slogo.Backend.LexicalAnalyzer;
 
+import java.util.HashMap;
 import java.util.Map;
 import slogo.Backend.State.CommandLanguage;
 
 public class TokenFactory {
   private static final RawTokenScanner RAW_TOKEN_SCANNER = RawTokenScanner.getTokenScanner();
-  private static Map<CommandLanguage, TokenEvaluator> tokenEvaluators;
+  private static Map<CommandLanguage, TokenEvaluator> tokenEvaluators = new HashMap<>();
 
   public static RawToken getRawToken(String s) throws InvalidTokenException {
     return RAW_TOKEN_SCANNER.attemptMatch(s);
